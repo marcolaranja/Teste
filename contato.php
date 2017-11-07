ab91b618b835462b7b5f77598da671ecfc207578
marcolaranja <32718901+marcolaranja@users.noreply.github.com>
2017-11-07 17:22:47 -0200

    update

--- contato.php
+++ contato.php
@@ -7,6 +7,35 @@
 </head>
 <body>
 
+		<nav class="navbar navbar-default">
+
+		<div class="container-fluid">
+			
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="#">Impacto</a>
+			</div>
+
+			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+				<ul class="nav navbar-nav navbar-right">
+					<li><a href="#">Home</a></li>
+					<li><a href="#">Quem Somos</a></li>
+					<li><a href="#">Portfólio</a></li>
+					<li><a href="#">Serviços</a></li>
+					<li><a href="#">Clientes</a></li>
+					<li><a href="#">Contato</a></li>
+				</ul>
+			</div>
+
+		</div>
+
+	</nav>
+
 	 <?php include 'header.php'; ?>
 
 
