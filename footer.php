b307be67f87da9c29b3f5a565888b145159b15a1
rafaelfsouza <rafael@impacto.online>
2017-11-07 19:45:31 -0200

    introdução jquery

--- footer.php
+++ footer.php
@@ -17,5 +17,22 @@
 
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
   <script src="./bootstrap/js/bootstrap.js"></script>
+  <script type="text/javascript">
+    
+
+    $(document).ready(function(){
+
+      $('#cadastro').submit(function(){
+
+        if($('#nome').val() == '' || $('#email').val() == '' || $('#idade').val() == ''){
+          alert('Todos os campos são obrigatórios');
+          return false;
+        }
+
+      });
+
+    });
+
+  </script>
 </body>
 </html>
--- index.php
+++ index.php
@@ -1,11 +1,49 @@
+<?php include 'header.php'; ?>
 
+<div class="container">
 
- <?php include 'header.php'; ?>
+	<h1>Home</h1>
 
-	<div class="container">
-		
-		<h1>Título da Página</h1>
+	<form id="cadastro">
 
-	</div>
+		<div class="row">
 
- <?php include 'footer.php'; ?>
+			<div class="col-md-5">
+				<div class="form-group">
+					<label>Nome:</label>
+					<input type="text" name="nome" id="nome" class="form-control" placeholder="Rafael">
+				</div>
+			</div>
+
+			<div class="col-md-5">
+				<div class="form-group">
+					<label>E-mail:</label>
+					<input type="email" name="email" id="email" class="form-control" placeholder="john@doe.com">
+				</div>
+			</div>
+
+			<div class="col-md-2">
+				<div class="form-group">
+					<label for="idade">Idade</label>
+					<input type="number" class="form-control" id="idade" placeholder="30">
+				</div>
+			</div>
+
+		</div>
+
+		<div class="row">
+
+			<div class="col-md-4 col-md-offset-4">
+				<button class="btn btn-success btn-lg btn-block" id="enviar">Enviar</button>
+			</div>
+
+		</div>
+
+	</form>
+
+</div>
+
+<br clear="all" />
+<br clear="all" />
+
+<?php include 'footer.php'; ?>
